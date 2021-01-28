549bda6d78dc546dc13627f1832ec6a23207f2e7
tonKristall <krisale@yandex.ru>
2021-01-28 18:13:32 +0300

    feat: test pass 7ms

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openbrarr = [];
+  let closebrarr = [];
+  let needclose = [];
+  for (i = 0; i < bracketsConfig.length; i++) {
+    let config = [];
+    config = bracketsConfig[i];
+    openbrarr.push(config[0]);
+    closebrarr.push(config[1]);
+  }
+  let strarr = str.split("");
+  for (i = 0; i < strarr.length; i++) {
+    if (openbrarr.includes(strarr[i])) {
+      if (closebrarr.includes(strarr[i])) {
+        if (!needclose.includes(strarr[i])) {
+          needclose.push(strarr[i]);
+        } else if (needclose[needclose.length - 1] == strarr[i]) {
+          needclose.pop();
+        } else {
+          return false;
+        }
+      } else {
+        needclose.push(strarr[i]);
+      }
+    } else if (closebrarr.includes(strarr[i])) {
+      let n = closebrarr.indexOf(strarr[i]);
+      if (needclose[needclose.length - 1] == openbrarr[n]) {
+        needclose.pop();
+      }
+      else {
+        return false;
+      }
+    }
+  }
+  if (needclose.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
